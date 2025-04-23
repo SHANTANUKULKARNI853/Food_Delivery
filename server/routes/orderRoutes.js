be8e9431a5fd414cfb70540b30abdbdfd9fc97b0
SHANTANU KULKARNI <163411519+SHANTANUKULKARNI853@users.noreply.github.com>
2025-04-23 16:34:25 +0530

    Delete server directory

--- server/routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createOrder, getUserOrders, getAllOrders } = require('../controllers/orderController');
-const router = express.Router();
-
-router.post('/create', createOrder);
-router.get('/user/:userId', getUserOrders);
-router.get('/admin/all', getAllOrders);
-
-module.exports = router;
